76564e9102e67ca98276dde6a3f4a74aa1770241
Vladimir <vladsmir323@gmail.com>
2022-07-30 07:17:32 +0300

    Task_1

--- Task_1.py
+++ Task_1.py
@@ -1,7 +1,7 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
 
-text = '/Users/inferno163/Documents/Учеба/ДЗ/2 semestr/Python/Seminar(python)_5.py/1_start_text_file1.txt'
+text = '1_start_text_file1.txt'
 text_file = open(text,'r')
 txt = text_file.read()
 print(txt)
@@ -13,7 +13,7 @@ def redak_text(txt):
 txt = redak_text(txt)
 print (txt)
 
-new_text_file = '/Users/inferno163/Documents/Учеба/ДЗ/2 semestr/Python/Seminar(python)_5.py/1_end_text.txt'
+new_text_file = '1_end_text.txt'
 new_text = open(new_text_file,'w')
 new_text.write(txt)
 new_text.close()
